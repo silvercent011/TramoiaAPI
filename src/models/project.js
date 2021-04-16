fb57260f4106c767054e0b9d49a7376479a0f398
brunoMSts <bms3@cin.ufpe.br>
2021-04-16 02:28:06 -0300

    Project schema completo | recuperação das estatísticas e engajamento dos projetos

--- src/models/project.js
+++ src/models/project.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ProjectSchema = new Schema({
-
+  _id: {type: String, required: true, unique: true},
+  statistics: {type: Object, default: null},
+  engagement: {type: Object, default: null}
 })
 
 module.exports = mongoose.model('Project', ProjectSchema, 'projects');
--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios')
 const router = express.Router();
+const Project = require('../models/project')
 
 router.get('/', async (req, res) => {
     //Obtêm os dados do body
@@ -10,8 +11,29 @@ router.get('/', async (req, res) => {
 
         //Obtendo apartir do token JWT todos projetos que o usuario participa
         const userProject = await axios.get(`${process.env.APIBASE}/projects/v1/project/${id}`, {headers:{"Authorization":`Bearer ${access_token}`}})
+        //Obtendo estatísticas e engajamento do projeto
+        const projectStatistics = await axios.get(`${process.env.APIBASE}/projects/v1/project/${id}/statistics`, {headers:{"Authorization":`Bearer ${access_token}`}})
+        const projectEngagement = await axios.get(`${process.env.APIBASE}/projects/v1/project/${id}/content-engagement`, {headers:{"Authorization":`Bearer ${access_token}`}})
+
         const project = userProject.data
-        return res.send(project)
+        
+        const finalData = {
+            _id: project.id,
+            statistics: projectStatistics.data, 
+            engagement: projectEngagement.data,
+        }
+
+        //Verifica se o projeto existe, se não existir, a gente cria
+        
+        if(await Project.findOne({_id: project.id})){
+            const userProject = await Project.findOne({_id: project.id})
+            return res.send(userProject)
+        } else {
+            const projectCreated = await Project.create(finalData)
+            return res.send(projectCreated)
+        }
+
+        //return res.send(project)
     } catch (error) {
         //Retorna erros se os métodos acima falharem
         return res.send({error:error})
